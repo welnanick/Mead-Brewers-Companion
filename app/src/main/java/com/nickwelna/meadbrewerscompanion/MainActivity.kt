62a05da4b818468d5158a8f9f0b7896c0c0d6f02
Nick Welna <welnanick@gmail.com>
2022-11-22 19:40:17 -0800

    Finish initial pass on Potential Alcohol Calc

--- app/src/main/java/com/nickwelna/meadbrewerscompanion/MainActivity.kt
+++ app/src/main/java/com/nickwelna/meadbrewerscompanion/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.nickwelna.meadbrewerscompanion
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,12 +32,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.focus.onFocusEvent
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nickwelna.meadbrewerscompanion.calculators.ABVCalculatorUtil.calcABV
 import com.nickwelna.meadbrewerscompanion.ui.theme.MeadBrewersCompanionTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,21 +48,32 @@ class MainActivity : ComponentActivity() {
             MeadBrewersCompanionTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {}
                 ABVCalc()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MeasurementInput(
+    value: String,
+    label: String,
+    onValueChange: (String) -> Unit,
+    onFocusChange: (FocusState) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        label = { Text(label) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .onFocusChanged(onFocusChange),
     )
 }
 
@@ -63,86 +84,142 @@ fun ABVCalc() {
     var finalGravity by remember { mutableStateOf("0.000") }
     var abv by remember { mutableStateOf("") }
 
+    val options = listOf("Specific Gravity", "BRIX", "Baume")
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf(options[0]) }
+
+
     Column(modifier = Modifier.padding(top = 16.dp)) {
+        // We want to react on tap/press on TextField to show menu
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            OutlinedTextField(
+                // The `menuAnchor` modifier must be passed to the text field for correctness.
+                modifier = Modifier
+                    .menuAnchor()
+                    .fillMaxWidth(),
+                readOnly = true,
+                value = selectedOptionText,
+                onValueChange = {},
+                label = { Text("Input Unit") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                options.forEach { selectionOption ->
+                    DropdownMenuItem(
+                        text = { Text(selectionOption) },
+                        onClick = {
+                            selectedOptionText = selectionOption
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                }
+            }
+        }
 
+        Spacer(modifier = Modifier.height(16.dp))
 
-        TextField(
+        MeasurementInput(
             value = originalGravity,
+            "Original Gravity",
             onValueChange = {
                 if (it.length <= 5) {
                     originalGravity = it
                 }
             },
-            singleLine = true,
-            label = { Text("Original Gravity") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-                .onFocusChanged {
-                    originalGravity =
-                        if (!it.hasFocus && originalGravity.isEmpty()) "0.000" else originalGravity
-                }
+            onFocusChange = {
+                originalGravity =
+                    if (!it.hasFocus && originalGravity.isEmpty()) "0.000" else originalGravity
+            })
 
-        )
         Spacer(modifier = Modifier.height(16.dp))
 
-        TextField(
+        MeasurementInput(
             value = finalGravity,
+            "Final Gravity",
             onValueChange = {
                 if (it.length <= 5) {
                     finalGravity = it
                 }
             },
-            singleLine = true,
-            label = { Text("Final Gravity") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-                .onFocusChanged {
-                    finalGravity =
-                        if (!it.hasFocus && finalGravity.isEmpty()) "0.000" else finalGravity
-                }
-
-        )
+            onFocusChange = {
+                finalGravity =
+                    if (!it.hasFocus && finalGravity.isEmpty()) "0.000" else finalGravity
+            })
         Spacer(modifier = Modifier.height(16.dp))
+        val radioOptions = listOf("ABV", "ABW")
+        val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
 
-        Button(onClick = {abv = "%.2f".format(calcABV(originalGravity.toFloat(), finalGravity.toFloat()))},
-            Modifier
-                .align(Alignment.End)
-                .padding(end = 16.dp)) {
-            Text("Calculate ABV")
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+
+            // Note that Modifier.selectableGroup() is essential to ensure correct accessibility behavior
+            Row(Modifier.selectableGroup()) {
+                radioOptions.forEach { text ->
+                    Row(
+                        Modifier
+                            .height(56.dp)
+                            .selectable(
+                                selected = (text == selectedOption), 
+                                onClick = { onOptionSelected(text) },
+                                role = Role.RadioButton
+                            )
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = (text == selectedOption),
+                            onClick = null // null recommended for accessibility with screenreaders
+                        )
+                        Text(
+                            text = text,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier.padding(start = 16.dp)
+                        )
+                    }
+                }
+            }
+            Button(
+                onClick = {
+                    abv = "%.2f".format(calcABV(originalGravity.toFloat(), finalGravity.toFloat()))
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(end = 16.dp)
+            ) {
+                Text("Calculate ABV")
+            }
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Calculated ABV is $abv", modifier = Modifier.padding(start = 16.dp), color = Color.White)
+        Text(
+            text = "Calculated ABV is $abv",
+            modifier = Modifier.padding(start = 16.dp),
+            color = MaterialTheme.colorScheme.onSurface
+        )
     }
 }
 
-fun calcABV(originalGravity: Float, finalGravity: Float): Float {
-
-    return specificGravityToAlcoholByVolume(1+originalGravity-finalGravity)
-
-}
-
-fun specificGravityToAlcoholByVolume(specificGravity: Float): Float {
-    return specificGravityToBaume(specificGravity)
-}
-
-fun specificGravityToBaume(specificGravity: Float): Float {
-    return  145*(1-(1/specificGravity))
-}
-
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
+)
 @Composable
 fun GreetingPreview() {
     MeadBrewersCompanionTheme {
         // A surface container using the 'background' color from the theme
         Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-        }
+            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+        ) {}
         ABVCalc()
     }
 }
